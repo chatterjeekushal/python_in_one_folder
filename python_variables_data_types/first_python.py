8fa3c1e7184800a5fa9f4608b8d00b8a2892a936
chatterjeekushal <chatterjeekushal89@gmail.com>
2024-03-08 12:56:06 +0530

    first python program

---
+++ python_variables_data_types/first_python.py
@@ -0,0 +1,185 @@
+
+
+
+# print hello world in python
+
+print("hello world")
+
+
+print("my name is rahul","my age is 23")
+
+
+
+# print number in python
+
+print(23)
+
+print(10+10)
+
+
+#Variables
+
+name="rahul"
+
+age=19
+
+price=25.99
+
+age2=age # age value set age2 
+
+print(name,age2 ,price)
+
+
+# type print python
+
+admin=True
+
+data=None
+
+
+print(type(name)) # str
+
+print(type(age))  #int
+
+print(type(price)) # float
+
+
+print(type(admin)) #bool  
+
+print(type(data))  # none type
+
+
+# print sum in python
+
+
+f_num=2
+s_num=5
+
+sum=f_num+s_num 
+
+print("sum of number is ",sum)
+
+
+
+
+num1=5
+num2=5
+
+print(num1+num2)#Arithmetic operators
+
+num1+=num2 #Assignment operators
+
+print(num1)
+
+print(num1==num2) #Comparison operators
+
+
+print(num1<num2 or num1==num2)  # logical 
+
+
+# type conversing
+
+num3=2
+
+num4=4.25
+
+sum=num3+num4 # 2.0 + 4.25 =>6.25
+
+print(sum)
+
+
+# type casting
+
+num5="4"
+
+num6=7
+
+print(int(num5)+7)  # int function convart string to number
+
+
+# convart string
+
+a=4
+a=str(a)
+
+print(type(a))
+
+
+
+#input in python
+
+# name=float(input("enter your name"))
+
+# print(name)
+
+
+# user full input
+
+yourname=input("enter your name :")
+
+yourage=int(input("enter your age"))
+
+marks=float(input("enter your marks"))
+
+
+print("your name is ", yourname)
+print("your age is ",yourage)
+print("your marks is ", marks)
+
+
+
+#  practice quation
+
+# write a program to input 2 numbers and print their sum
+
+fast_input=int(input("enter your number :"))
+secend_input=int(input("enter your number :"))
+
+sum = fast_input+secend_input
+
+print(sum)
+
+
+# wap to input side a square and print its area
+
+side1=float(input("enter your side"))
+side2=float(input("enter your side"))
+
+area=side1*side2
+
+print(area)
+
+
+# wap to input 2 int numbers and a and b print true if a is greater than or equal to b if not print false
+
+a=int(input("enter your int num : "))
+b=int(input("enter your int num : "))
+
+
+print(a>=b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
