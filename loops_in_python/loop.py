e627a1f624dd22a2cacc868a1363333d6b61d1cc
chatterjeekushal <chatterjeekushal89@gmail.com>
2024-03-26 22:31:05 +0530

    add new folder

---
+++ loops_in_python/loop.py
@@ -0,0 +1,88 @@
+
+
+
+# while loop in python
+
+# count=1
+
+# while count<=5 :
+
+#     print("hello world")
+
+#     count+=1
+
+
+
+# i=10
+
+# while i>=0:
+#         print(i)
+#         i-=1
+
+
+
+
+# practic quation for while loop
+
+# print number from 1 to 100
+
+
+# r=1
+
+# while r<=100:
+#     print(r)
+#     r+=1
+
+
+
+# print number from 100 to 1
+
+
+# j=100
+
+# while j>=1:
+#     print(j)
+#     j-=1
+
+
+
+# print the multipication table of number n
+
+# n=1
+
+# h=0
+# while n<=10:
+    
+#     h=h+n
+    
+#     n+=1
+
+# print(h)
+
+
+
+# print the elements of the following list using a loop
+
+#list1=[1,4,9,16,25,36,49,64,81,100]
+
+
+# l=0
+
+# while l<len(list1):
+#     print(list1[l])
+#     l+=1
+
+
+
+
+g=0
+
+tr=(2,4,6,8,6)
+
+while g<len(tr):
+    if(tr[g]==4):
+        print("4 is exjist",g)
+    g+=1
+
+
+
